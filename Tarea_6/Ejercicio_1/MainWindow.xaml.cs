4d65bd39d3a5f214ec861e65d4e94c847f816a5a
Francoborsotti <francoborsotti@hotmail.com>
2025-09-03 16:44:13 -0300

    Ejercicio 1 terminado

---
+++ Tarea_6/Ejercicio_1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double resultado;
+            if (double.TryParse(grados.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out resultado))
+            {
+                mensajeError.Content = "";
+                resultado = double.Parse(grados.Text);
+                resultado *= 1.8;
+                resultado += 32;
+
+                labelResultado.Content = resultado.ToString();
+            }
+            else
+            {
+                mensajeError.Content = "Introduzca un número!!";
+            }
+        }
+    }
+}
